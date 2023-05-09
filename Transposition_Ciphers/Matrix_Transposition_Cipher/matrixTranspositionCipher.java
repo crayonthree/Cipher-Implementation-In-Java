e6950e9bc23b52a47f67945edc7843c5e293ee28
crayonthree <lactosetheintolerant2@yahoo.com>
2023-05-08 21:59:24 -0400

    Matrix Transposition Encryption

--- Transposition_Ciphers/Matrix_Transposition_Cipher/matrixTranspositionCipher.java
+++ Transposition_Ciphers/Matrix_Transposition_Cipher/matrixTranspositionCipher.java
@@ -1,3 +1,8 @@
+/*
+ * Name: matrixTranspositionCipher.java
+ * Author: crayonthree / Abhiroop Yerramilli
+ * Description: This class contains the functions to encrypt and decrypt a given string based on the entered key, using matrix transposition cipher.
+ */
 package Transposition_Ciphers.Matrix_Transposition_Cipher;
 
 public class matrixTranspositionCipher {
@@ -12,8 +17,11 @@ public class matrixTranspositionCipher {
      * Description: This constructor takes in a key, and makes the key array.
      */
     public matrixTranspositionCipher(String key){
+
+        //Replacing spaces from the key.
         key = key.replaceAll(" ", "");
 
+        //Initializing the column key, using the ASCII table.
         columnKey = new int[key.length()];
         for(int i=0;i<key.length();i++){
             columnKey[i] = (int)key.charAt(i) - 49;
@@ -27,7 +35,54 @@ public class matrixTranspositionCipher {
      * Description: This function takes in a string to be encrypted, and encrypts it using the matrix transposition cipher.
      */
     public String matrixTranspositionEncryption(String textToBeEncrypted){
-        return null;
+        
+        //Parsing the input string.
+        textToBeEncrypted = textToBeEncrypted.replaceAll(" ", "%");
+
+        //Initializing ints to store variables for the matrix.
+        int numberOfColumns = columnKey.length;
+        int numberOfCharacters = textToBeEncrypted.length();
+        int numberOfRows = (numberOfCharacters/numberOfColumns) + 1;
+
+        //Making the matrix
+        char[][] currentPlaintextMatrix = new char[numberOfRows][numberOfColumns];
+
+        int currentCharacterIndex = 0;
+        for(int i=0;i<numberOfRows;i++){
+            for(int j=0;j<numberOfColumns;j++){
+
+                //While there are elements in the string, use those characters, else use '%'
+                if(currentCharacterIndex<numberOfCharacters){
+
+                    currentPlaintextMatrix[i][j] = textToBeEncrypted.charAt(currentCharacterIndex);
+                    currentCharacterIndex++;
+
+                }else{
+
+                    currentPlaintextMatrix[i][j] = '%';
+
+                }
+
+            }
+        }
+
+        //Applying the key for encryption
+        String cipherText = "";
+        for(int j=0;j<columnKey.length;j++){
+
+            String currentColumn = "";
+            for(int i=0;i<numberOfRows;i++){
+
+                currentColumn = currentColumn + currentPlaintextMatrix[i][columnKey[j]];
+
+            }
+
+            //Reading the columns in order of the columns and adding them.
+            cipherText = cipherText + currentColumn;
+        }
+
+        //Returning the cipherText
+        return cipherText;
     }
 
     /*
